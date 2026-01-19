73f2c85ac4872fe57b33ffdb5febf5ca5f76a086
Pankaj Kumar <pankajkumar732298@gmail.com>
2026-01-19 21:27:21 +0530

    change in the file production.py

--- core/settings/production.py
+++ core/settings/production.py
@@ -2,7 +2,9 @@ from .base import *
 import dj_database_url
 
 DEBUG = False
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.0.161','www.coolnira.in']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.0.161','www.coolnira.in','coolnira.onrender.com']
+# ALLOWED_HOSTS = ['www.coolnira.in','coolnira.onrender.com']
+
 # WhiteNoise (recommended)
 MIDDLEWARE.insert(
     1,
