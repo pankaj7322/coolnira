9341feb7ffb88c268a4b150a12a90cd0a1595cee
Pankaj Kumar <pankajkumar732298@gmail.com>
2026-01-19 21:57:35 +0530

    change in the file production.py

--- core/settings/production.py
+++ core/settings/production.py
@@ -1,25 +1,21 @@
 from .base import *
-import dj_database_url
+import os
+from pathlib import Path
 
 DEBUG = False
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.0.161','www.coolnira.in','coolnira.onrender.com']
-# ALLOWED_HOSTS = ['www.coolnira.in','coolnira.onrender.com']
 
-# WhiteNoise (recommended)
-MIDDLEWARE.insert(
-    1,
-    'whitenoise.middleware.WhiteNoiseMiddleware'
-)
-DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
-    )
-}
+ALLOWED_HOSTS = ['coolnira.in', 'www.coolnira.in', 'coolnira.onrender.com']
 
+# Whitenoise for serving static files
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Static files (CSS, JS, Images)
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# Media files (uploads)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
