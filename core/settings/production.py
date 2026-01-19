f5c873b1d98acc3024788f2f41a729258db081a5
Pankaj Kumar <pankajkumar732298@gmail.com>
2026-01-19 20:39:04 +0530

    add dj-database-url

--- core/settings/production.py
+++ core/settings/production.py
@@ -1,4 +1,5 @@
 from .base import *
+import dj_database_url
 
 DEBUG = False
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.0.161','www.coolina.in']
@@ -7,5 +8,10 @@ MIDDLEWARE.insert(
     1,
     'whitenoise.middleware.WhiteNoiseMiddleware'
 )
+DATABASES = {
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+    )
+}
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
